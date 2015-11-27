291fd6ae118d7399ee5da08ac77a2dba748b079f
veeru2015 <yoursbiru@hotmail.com>
2015-11-27 10:44:33 +0545

    Migration Added fot User n User_type

---
+++ database/migrations/2015_11_27_044402_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fname');
+            $table->string('mname')->nullable();
+            $table->string('lname')->nullable();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->enum('gender',['f','m','o']);
+            $table->string('ip');
+            $table->integer('user_typeId')->unsigned();
+            $table->foreign('user_typeId')->references('id')->on('user_types');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
