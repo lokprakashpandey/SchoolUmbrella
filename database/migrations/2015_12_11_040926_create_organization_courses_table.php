3c7b3709ddc910c5b911a691448ac696a790debf
veeru2015 <yoursbiru@hotmail.com>
2015-12-11 10:17:20 +0545

    Tables are added

---
+++ database/migrations/2015_12_11_040926_create_organization_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('organization_courses');
+        Schema::create('organization_courses', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('organizationId')->unsigned();
+            $table->foreign('organizationId')->references('id')->on('organization_profiles');
+            $table->integer('courseId')->unsigned();
+            $table->foreign('courseId')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('organization_courses');
+    }
+}
