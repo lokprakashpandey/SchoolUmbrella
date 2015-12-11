d8478c29aab592ca373e72dd60163f33387d55a0
Prem Singh <prem.singh@peacenepal.com>
2015-12-11 11:25:15 +0545

    countries table changed

--- database/migrations/2015_12_11_030644_create_countries_table.php
+++ database/migrations/2015_12_11_030644_create_countries_table.php
@@ -14,6 +14,7 @@ class CreateCountriesTable extends Migration
     {
         Schema::create('countries', function(Blueprint $table){
             $table->increments('id');
+            $table->string('code');
             $table->string('name');
         });
     }
