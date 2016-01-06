4bcbfe43970c0fec3642f3f3daa49557f03c87ba
Prem Singh <prem.singh@peacenepal.com>
2016-01-06 10:15:57 +0545

    console testing

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,4 @@
 function changeUserType(){
-
 	document.getElementById('extraInfo').style.display = (document.getElementById('user_typeId').value == 5)? 'none' : 'block';
 	document.getElementById('forOrg').style.display = (document.getElementById('user_typeId').value == 5)? 'block' : 'none';
 
