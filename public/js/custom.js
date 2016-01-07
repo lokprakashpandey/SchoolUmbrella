7400d5c282a5ac360c9cc1f33b6da52e9dcabb60
Prem Singh <prem.singh@peacenepal.com>
2016-01-07 09:56:31 +0545

    tables for notes added

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,4 @@
 function changeUserType(){
-	debugger;
-	console.log("TEST");
 	document.getElementById('organizationInfo').style.display = (document.getElementById('user_typeId').value == 5)? 'block':'none';
 	document.getElementById('extraInfo').style.display = (document.getElementById('user_typeId').value == 5)? 'none' : 'block';
 }
